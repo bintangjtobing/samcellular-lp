efe4ba38b1bca36843a4430aead9bafe6698b087
bahari <haribahari71@gmail.com>
2025-01-24 23:57:29 +0700

    Fix: Become Seller

--- src/app/become-seller/page.jsx
+++ src/app/become-seller/page.jsx
@@ -1,16 +1,4 @@
-import BottomFooter from "@/components/BottomFooter";
-import Breadcrumb from "@/components/Breadcrumb";
-import BreadcrumbImage from "@/components/BreadcrumbImage";
-import CounterSection from "@/components/CounterSection";
-import FooterTwo from "@/components/FooterTwo";
-import HeaderTwo from "@/components/HeaderTwo";
-import ShippingOne from "@/components/ShippingOne";
-import StepsSection from "@/components/StepsSection";
-import TestimonialOne from "@/components/TestimonialOne";
-import WhyBecomeSeller from "@/components/WhyBecomeSeller";
-import ColorInit from "@/helper/ColorInit";
-import Preloader from "@/helper/Preloader";
-import ScrollToTopInit from "@/helper/ScrollToTopInit";
+import PageComponents from "./PageComponents";
 
 export const metadata = {
   title: "Sam Cellular - Jual Beli HP Baru & Bekas | Kredit HP Cepat Cair",
@@ -21,46 +9,7 @@ export const metadata = {
 const page = () => {
   return (
     <>
-      {/* ColorInit */}
-      <ColorInit color={true} />
-
-      {/* ScrollToTop */}
-      <ScrollToTopInit color="#FA6400" />
-
-      {/* Preloader */}
-      <Preloader />
-
-      {/* HeaderTwo */}
-      <HeaderTwo category={true} />
-
-      {/* Breadcrumb */}
-      <Breadcrumb title={"Become Seller"} />
-
-      {/* BreadcrumbImage */}
-      <BreadcrumbImage />
-
-      {/* WhyBecomeSeller */}
-      <WhyBecomeSeller />
-
-      {/* CounterSection */}
-      <CounterSection />
-
-      {/* StepsSection */}
-      <StepsSection />
-
-      {/* TestimonialOne */}
-      <section className="pb-120">
-        <TestimonialOne />
-      </section>
-
-      {/* ShippingOne */}
-      <ShippingOne />
-
-      {/* FooterTwo */}
-      <FooterTwo />
-
-      {/* BottomFooter */}
-      <BottomFooter />
+     <PageComponents/>
     </>
   );
 };
